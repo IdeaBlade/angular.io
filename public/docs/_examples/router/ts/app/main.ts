a98460e63801b337531bf7092cc4689916787aab
Brandon Roberts <robertsbt@gmail.com>
2016-08-03 13:07:17 -0500

    Fixed broken e2e tests for router

--- public/docs/_examples/router/ts/app/main.ts
+++ public/docs/_examples/router/ts/app/main.ts
@@ -5,11 +5,15 @@ import { FormsModule }  from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { routes }       from './app.routes';
+import { routes,
+         appRouterProviders } from './app.routes';
 
 bootstrap(AppComponent, {
   imports:   [
     FormsModule,
     RouterModule.forRoot(routes, {})
+  ],
+  providers: [
+    appRouterProviders
   ]
 });
