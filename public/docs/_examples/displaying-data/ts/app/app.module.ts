119bdc61465a39f18d6abede1f4af58c9ec5e349
Jesús Rodríguez <Foxandxss@gmail.com>
2016-08-03 22:05:09 +0200

    add displaying data

---
+++ public/docs/_examples/displaying-data/ts/app/app.module.ts
@@ -0,0 +1,20 @@
+// #docregion
+import { NgModule, ApplicationRef } from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports: [
+    BrowserModule
+  ],
+  declarations: [
+    AppComponent
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule {
+  constructor(appRef: ApplicationRef) {
+    appRef.bootstrap(AppComponent);
+  }
+}
