fc955b3c0634f82eb1226171727387e0b8167834
Filipe Silva <filipematossilva@gmail.com>
2016-08-04 00:00:10 +0100

    chore: remove unused code from quickstart

--- public/docs/_examples/quickstart/ts/app/app.module.ts
+++ public/docs/_examples/quickstart/ts/app/app.module.ts
@@ -3,21 +3,9 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './app.component';
 
-//  Soon
-// @NgModule({
-//   imports: [ BrowserModule ],
-//   declarations: [ AppComponent ],
-//   bootstrap: [ AppComponent ]
-// })
-// export class AppModule { }
-
-
-// For Now
-import { ApplicationRef } from '@angular/core';
 @NgModule({
   imports: [ BrowserModule ],
   declarations: [ AppComponent ],
-  entryComponents: [ AppComponent ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
