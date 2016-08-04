3b4eb65f11bf903e9dece437835e6f5a2f1554b3
Torgeir Helgevold <torgeirh@hotmail.com>
2016-08-03 21:12:13 -0400

    update main

--- public/docs/_examples/cb-dependency-injection/ts/app/main.ts
+++ public/docs/_examples/cb-dependency-injection/ts/app/main.ts
@@ -1,26 +1,5 @@
 // #docregion
-import { bootstrap }            from '@angular/platform-browser-dynamic';
-import { FormsModule }          from '@angular/forms';
-import { XHRBackend }           from '@angular/http';
-import { appRouterProviders }   from './app.routes';
-import { LocationStrategy,
-         HashLocationStrategy } from '@angular/common';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
 
-import { HeroData }         from './hero-data';
-import { InMemoryBackendService,
-         SEED_DATA }        from 'angular2-in-memory-web-api';
-
-import { AppComponent }     from './app.component';
-
-// #docregion bootstrap
-bootstrap(AppComponent, {
-  imports: [FormsModule],
-  providers: [
-    appRouterProviders,
-    { provide: LocationStrategy, useClass: HashLocationStrategy },
-
-    { provide: XHRBackend, useClass: InMemoryBackendService }, // in-mem server
-    { provide: SEED_DATA,  useClass: HeroData } // in-mem server data
-  ]
-});
-// #enddocregion bootstrap
+platformBrowserDynamic().bootstrapModule(AppModule);
