90c5925c9f3bc002f5d1e5fcc9cf7daf80e92db5
Filipe Silva <filipematossilva@gmail.com>
2016-08-04 01:54:04 +0100

    chore: update animations chapter

---
+++ public/docs/_examples/animations/ts/app/app.module.ts
@@ -0,0 +1,10 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HeroTeamBuilderComponent } from './hero-team-builder.component';
+
+@NgModule({
+  imports: [ BrowserModule ],
+  declarations: [ HeroTeamBuilderComponent ],
+  bootstrap: [ HeroTeamBuilderComponent ]
+})
+export class AppModule { }
